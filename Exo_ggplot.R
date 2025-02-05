45b32641563d57b1227718f1c4c75ebc933c5523
Clara2403-open <ccouston@etu.isara.fr>
2025-02-05 10:34:13 +0100

    Update data

--- Exo_ggplot.R
+++ Exo_ggplot.R
@@ -23,8 +23,6 @@ ggplot(data=iris, mapping = aes(x=Species, y=Sepal.Length,
   theme_dark()
 
 library(dplyr)
-data("starwars")
-starwars
 data("diamonds")
 diamonds
 
@@ -32,9 +30,31 @@ geom_contour_filled(mapping = aes(x=diamonds$color, y=diamonds$depth), data = di
 geom_hex(mapping = aes(x=diamonds$color, y=diamonds$depth),data = diamonds,stat = "binhex",position = "identity",show.legend = TRUE,inherit.aes = TRUE)
 geom_bar(mapping = aes(x = diamonds$carat,y=diamonds$price), data = diamonds, just = 0.5,width = NULL)
 
-f <- ggplot(mpg, aes(diamonds$carat, diamonds$price))
+f <- ggplot(diamonds, aes(carat, price))
 f + geom_hex(mapping = aes(x=color, y=depth),data = diamonds,stat = "binhex",position = "identity",show.legend = TRUE,inherit.aes = TRUE, col="red", pch=16)
 
-f + geom_area(mapping = aes(x=carat, y=price),data=diamonds, stat = "align", position = "stack",outline.type = "upper", show.legend = TRUE, rainbow(n,s=1,v=1)
+library(ggplot2)
 
+data("diamonds")
+
+# Création d'un graphique avancé avec plusieurs dimensions
+plot <- ggplot(diamonds, aes(x = carat, y = price, color = cut, size = depth, shape = clarity)) +
+  geom_point(alpha = 0.6) +  # Ajout de points avec une transparence de 60%
+  labs(
+    title = "Analyse avancée des diamants",
+    subtitle = "Relation entre le carat, le prix, la coupe, la profondeur et la clarté",
+    x = "Carat",
+    y = "Prix (USD)",
+    color = "Qualité de la coupe",
+    size = "Profondeur",
+    shape = "Clarté"
+  ) +
+  theme_minimal() +  # Application d'un thème épuré
+  scale_color_manual(values = c("Fair" = "red", "Good" = "blue", "Very Good" = "green", "Premium" = "purple", "Ideal" = "orange")) +
+  scale_size_continuous(range = c(1, 5)) +  # Ajustement de la taille des points
+  facet_wrap(~ cut) +  # Séparation en sous-graphiques par qualité de coupe
+  theme(legend.position = "bottom", panel.grid.major = element_line(color = "grey90"))
+
+# Affichage du graphique
+print(plot)
               
